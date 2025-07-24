04a3370d163f69653b5814d35c19175ec9dc6b46
kimyoungsik <dean4208@gmail.com>
2025-07-24 17:01:31 +0900

    5

--- src/test/java/mission2/SteeringTest.java
+++ src/test/java/mission2/SteeringTest.java
@@ -1,30 +1,35 @@
 package mission2;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class SteeringTest {
     Steering steering = Steering.getInstance();
 
     @Test
+    @DisplayName("Steering id 가져오기")
     void getSteeringId() {
         steering.setSteeringId(1);
         Assertions.assertThat(steering.getSteeringId()).isEqualTo(1);
     }
 
     @Test
-    void getSteeringName() {
+    @DisplayName("Steering name Bosch")
+    void getSteeringName_Bosch() {
         steering.setSteeringId(1);
         Assertions.assertThat(steering.getSteeringName()).isEqualTo("Bosch");
     }
 
     @Test
+    @DisplayName("Steering name MOBIS")
     void getSteeringName_MOBIS() {
         steering.setSteeringId(2);
         Assertions.assertThat(steering.getSteeringName()).isEqualTo("Mobis");
     }
 
     @Test
+    @DisplayName("Steering name UNKNOWN")
     void getSteeringName_Unknown() {
         steering.setSteeringId(3);
         Assertions.assertThat(steering.getSteeringName()).isEqualTo("알 수 없는 Steering 시스템");
